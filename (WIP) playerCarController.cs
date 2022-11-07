2d0dc4212f70689d11b3ac91bb4240bee6e3c168
Julian-Herrera <jherreraperez@lshigh.org>
2022-11-06 21:48:18 -0800

    Update (WIP) playerCarController.cs

--- (WIP) playerCarController.cs
+++ (WIP) playerCarController.cs
@@ -2,66 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/*
-NOTE To Self:
-Add velocity to the car to add inertia, so the car does not 
-just stop when the player stops pressing the key.
-*/
-
-
-
 public class playerControl : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float turnSpeed = 1.5f;
-    public Vector3 lastPosition = Vector3.zero;
+    public float moveSpeed = 1.0f;
+    public float turnSpeed = 2.5f;
+    private int i = 0;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
-        //move forwrd
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-        }
-        //move backward
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
-        }
-        //turn left
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.up, -turnSpeed * moveSpeed*Time.deltaTime);
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
-        }
-        //turn right
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(Vector3.up, turnSpeed * moveSpeed*Time.deltaTime);
-            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
-        }
-        //handbrake
-        if(Input.GetKey(KeyCode.Space))
-        {
-            for(int i=5; i>0; i--)
+            
+            rb.AddForce(transform.forward * moveSpeed);
+            while (i<=100)
             {
-                moveSpeed -=1f;
-                turnSpeed -=0.001f;
+                moveSpeed += 0.1f;
+                i++;
+            }
+            
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(-transform.forward * moveSpeed);
+            while (i >= 100)
+            {
+                moveSpeed -= 0.1f;
+                i--;
             }
         }
-        else
-        {
-            moveSpeed = 5.0f;
-            turnSpeed = 1.5f;
-        }
-        //boost
-        if(Input.GetKey(KeyCode.LeftShift))
-        {
-            moveSpeed = 10;
-        }
-        else
-        {
-            moveSpeed = 5;
-        }
-        
     }
 }
