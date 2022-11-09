0b3fffc6ed1146a7ff6c578c28e5c429bbf4b934
Julian-Herrera <jherreraperez@lshigh.org>
2022-11-08 19:36:28 -0800

    Update (WIP) playerCarController.cs

--- (WIP) playerCarController.cs
+++ (WIP) playerCarController.cs
@@ -6,7 +6,7 @@ public class playerControl : MonoBehaviour
 {
     public float moveSpeed = 1.0f;
     public float turnSpeed = 2.5f;
-    private int i = 0;
+    private int i = 100;
     private Rigidbody rb;
 
     void Start()
@@ -16,25 +16,44 @@ public class playerControl : MonoBehaviour
 
     void Update()
     {
+        Drive();
+    }
+
+
+
+    public void Drive()
+    {
         if (Input.GetKey(KeyCode.W))
         {
-            
             rb.AddForce(transform.forward * moveSpeed);
-            while (i<=100)
-            {
-                moveSpeed += 0.1f;
-                i++;
-            }
-            
+            moveSpeed += 0.01f;
         }
         else if (Input.GetKey(KeyCode.S))
         {
             rb.AddForce(-transform.forward * moveSpeed);
+            
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up, -turnSpeed *moveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, turnSpeed *moveSpeed* Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.Space))
+        {
+        
             while (i >= 100)
             {
                 moveSpeed -= 0.1f;
                 i--;
             }
         }
+        else
+        {
+            moveSpeed = 1.0f;
+        }
+        
     }
 }
